e21ff3cd7411dbb08a385524ec09bc683bddd2c8
Rogeriobg <studiorgonline@gmail.com>
2025-03-27 12:22:32 -0300

    projeto finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, seu bosta!");
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo = 0.0;
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        boolean saldoValido = false;
+        while (!saldoValido) {
+            System.out.print("Por favor, digite o saldo da Conta: ");
+            if (scanner.hasNextDouble()) {
+                saldo = scanner.nextDouble();
+                saldoValido = true;
+            } else {
+                System.out.println("Valor inválido para o saldo. Por favor, digite um número decimal.");
+                scanner.nextLine();
+            }
+        }
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo
+                + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
